ea0b4cf68738c96e687a31cf68672e8ff512b748
3eLeHblu <innocent87@mail.ru>
2023-10-23 13:13:35 +0300

    Java

---
+++ Sem1/src/main/java/org/example/seminar1/cw/task1.java
@@ -0,0 +1,10 @@
+//Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+package org.example.seminar1.cw;
+
+public class task1 {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ programm.java
@@ -0,0 +1,15 @@
+/**
+ * programm
+ */
+public class programm {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        a = --a-a--; 
+        
+
+        System.out.println(a);
+    }
+
+}
